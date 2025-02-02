8d1ae799cdd16604a0d756478ed7953672fb2c33
HectorAlejandro26 <alex2611alejandro@gmail.com>
2025-02-02 14:06:01 -0700

    3er commit. Corregido:  - Evaluacion de la funcion en la funcion "graph"

--- graph.py
+++ graph.py
@@ -3,22 +3,21 @@ import numpy as np
 from typing import Sequence
 
 
-def graph(coef: Sequence[int], roots: Sequence[int]) -> None:
+def graph(coef: Sequence[int], roots: Sequence[int], ranges: int = 1) -> None:
     def f(x):
         res = 0
-        for c in range(len(coef)):
-            res += coef[c] * (x ** (len(coef) - c))
+        for i, c in enumerate(coef):
+            res += c * (x ** (len(coef) - i - 1))
         return res
 
-    limMax = max(roots) + 1
-    limMin = min(roots) - 1
+    limMax = max(roots) + ranges
+    limMin = min(roots) - ranges
     spacing = ((abs(limMax) + abs(limMin))//4+1) * 100
     print(f"Spacing: {spacing}")
 
     x = np.linspace(limMin, limMax, spacing, endpoint=True)
     plt.plot(x, f(x), "r-")
     plt.scatter(roots, np.zeros_like(roots), color="blue")
-    plt.scatter(0, 0, color="black")
     plt.axhline(0, color="black", linewidth=0.8, linestyle=":")
     plt.axvline(0, color="black", linewidth=0.8, linestyle=":")
     plt.show()
